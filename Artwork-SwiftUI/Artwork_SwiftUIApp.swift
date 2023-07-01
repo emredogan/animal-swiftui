c5411f2f931216c2cd9cc460b5db4a95a911eb05
Emre Dogan <emre.dogan@shortcut.no>
2023-07-01 21:17:20 +0200

    Initial Commit

---
+++ Artwork-SwiftUI/Artwork_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Artwork_SwiftUIApp.swift
+//  Artwork-SwiftUI
+//
+//  Created by Emre Dogan on 01/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Artwork_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
